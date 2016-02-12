eb14c1da49d98cb82ac532ba9e4f331a0e4ca881
Gauffret <auffretgwladys@gmail.com>
2016-02-12 11:22:28 +0100

    V1.1

--- tp2/abr.h
+++ tp2/abr.h
@@ -1,4 +1,15 @@
+
+typedef int element;
+
+typedef struct noeud {
+	element valeur;
+	struct noeud *gauche, *droit;
+} NOEUD, *ABR;
+
+
 NOEUD* arbre_vide();    
+
 NOEUD* insere(NOEUD* p,element x);
+
 void affiche_arbre(NOEUD* p,int col);         
 
