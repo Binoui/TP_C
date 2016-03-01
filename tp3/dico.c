d57b79d09989e34239cebaee13bd3b0556cca335
Binoui <Binoui1@gmail.com>
2016-03-01 10:59:22 +0100

    dico ça rime avec poil au dos

--- tp3/dico.c
+++ tp3/dico.c
@@ -5,3 +5,65 @@ typedef struct
 	noeud * fils;
 } noeud;
 
+dico * insertion(noeud * dico, char * mot, int taille, int index)
+{
+	noeud * nouveau;
+
+	if (index < taille)
+	{
+		if (dico == NULL)
+		{
+			dico = malloc(sizeof(noeud));
+			dico->lettre = mot[index];
+
+			insertion(dico->fils, mot, taille, index+1);
+		}
+		else
+		{	
+			if (dico->lettre > mot[index])
+			{
+
+				if (dico->frere != NULL && dico->frere->lettre > mot[index+1])
+				{
+					nouveau = malloc(sizeof(noeud));
+					nouveau->lettre = mot[index+1];
+					nouveau->frere = dico->frere;
+					dico->frere = nouveau;
+				}	
+
+				insertion(dico->fils, mot, taille, index+1);)
+			}
+			else
+			{
+				insertion(dico->fils, mot, taille, index+1);
+			}
+		}
+	}
+	return dico;
+}
+
+void chargement(noeud * arbre, char * chemin)
+{
+	int i;
+	char * 
+	FILE * fich = fopen(chemin, "r");
+	
+	if (fich == NULL)
+		printf("Erreur fichier\n");
+	else
+	{
+		fprintf(chemin, "%d\n", &tailleDico);
+		for (i = 0; i < taille; i++)
+		{
+			fprintf(stderr, "%s\n", );
+
+		}
+	}
+}
+
+int main(int argc, char *argv[])
+{
+
+
+	return 0;
+}
