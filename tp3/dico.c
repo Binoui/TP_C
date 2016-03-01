0cf79a97d6781f685b1128ef159b1bdb63b6713d
Gauffret <auffretgwladys@gmail.com>
2016-03-01 10:58:38 +0100

    pouet pouet

--- tp3/dico.c
+++ tp3/dico.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 typedef struct
 {
 	char lettre;
@@ -5,3 +8,24 @@ typedef struct
 	noeud * fils;
 } noeud;
 
+
+noeud* creeDico (){
+	return NULL;
+}
+
+int recherche (char* clef, noeud* d,int taille, int index){
+	int trouve = 0;
+	if (d ==NULL)
+		return 0;
+
+	if (d->lettre != clef[index]) 
+		trouve = recherche (clef, d->frere, taille, index)
+	else{
+		index ++;
+		if (index == taille)
+			trouve = 1;
+		else
+			trouve = recherche (clef, d->fils, taille, index);
+	}
+	return trouve;
+} 
